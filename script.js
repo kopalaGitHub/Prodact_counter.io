5fe76900baff726f337404412f3ed3ff194a4876
sabaKopala <kopaleishviliGitHub@gmail.com>
2023-12-20 22:46:04 +0400

    update1.1

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // I tried but faild so many times :D
 });
+const warning = document.getElementById("warning");
+warning.style.display = "none";
 
-let x = prompt("Enter a Value", "0"); //Allows user to enter number
-document.getElementById("counter").innerHTML = x; // number will apear on page
-var timesClicked = parseInt(x, 10); // user's 'number' is string.
+let value = document.getElementById("counter");
+let x = prompt("Enter a Value", "30"); //Allows user to enter number\
+value.innerHTML = x; // number will apear on page
+let stringToNum = parseInt(x, 10); // user's 'number' is string.
+if (stringToNum > 100) {
+  value.innerHTML = "100";
+  stringToNum = 100;
+  alert("you can only choose 10 to 100");
+}
 
 function btnClickDown() {
   //On click function
-  timesClicked -= 10;
-  if (document.getElementById("counter").innerHTML < 10) {
-    alert("you cann't go any further");
+
+  if (stringToNum < 20) {
+    // alert("you cann't go any further");
+    warning.style.display = "block";
   } else {
-    document.getElementById("counter").innerHTML = timesClicked;
+    stringToNum -= 10;
+    value.innerHTML = stringToNum;
+    warning.style.display = "none";
   }
 }
 function btnClickUp() {
   //On click function
-  timesClicked += 10;
-  document.getElementById("counter").innerHTML = timesClicked;
+  if (stringToNum > 90) {
+    warning.style.display = "block";
+  } else {
+    stringToNum += 10;
+    value.innerHTML = stringToNum;
+    warning.style.display = "none";
+  }
 }
